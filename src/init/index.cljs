bfff9a028a4a36951f9288888be69b8034d227b9
jooter <bbb314@gmail.com>
2018-04-21 22:52:18 +1000

    use textarea to replace input

--- src/init/index.cljs
+++ src/init/index.cljs
@@ -3,35 +3,97 @@
   (:require [freactive.core :refer [atom]]
             [freactive.dom :as dom]
             #_[clojure.string :as s]
+            [cljs.pprint :refer [pprint]]
 
-            [cljs.reader :refer [read-string]]
-            [cljs.js :refer [empty-state eval js-eval]]
+            #_[cljs.env :as env]
+            #_[cljs.analyzer :as ana]
             #_[cljs.env :refer [*compiler*]]
-            #_[cljs.pprint :refer [pprint]]
+            [cljs.reader :refer [read-string]]
+            [cljs.js :refer [empty-state eval #_eval-str js-eval]]
 
+            #_[cljs.analyzer :as ana :include-macros true]
             #_[cljs.core.async :refer [put! chan <! >!]])
   (:require-macros #_[cljs.core.async.macros :refer [go go-loop]]
                    [freactive.macros :refer [rx]]))
 
-(defn eval-str [s]
+(defonce mousexy (atom nil))
+
+#_(defn eval-str-1
+    "This does not work"
+    [s]
+    (eval-str (empty-state) s "input"
+              {:eval js-eval}
+              identity
+              ))
+
+(defn eval-str-2 [s]
   (eval (empty-state)
         (read-string s)
         {:eval       js-eval
-         :source-map true
-         :context    :expr}
-        (fn [result] result)))
+         ; :source-map true
+         ; :ns 'cljs.core
+         ; :ns 'init.index
+         ; :verbose true
+         ; :load (fn [ a b ] (prn :load a b))
+         ; :load-macros true
+         ; :macros true
+         ; :context    :expr
+         }
+        #_(fn [result] result)
+        identity
+        ))
 
-#_(defn pp-str [form1]
+(defn pp-str [form1]
   (with-out-str
     (pprint form1)))
 
-(def example-input-1 "(do (js/alert 9) [:h1 \"Hi\"])")
+#_(def example-input-1 "(do (js/alert 9) [:h1 \"Hi\"])")
 
 #_(def example-input-1
   (pp-str
    '(do
       (js/alert 9)
       [:h1 "Hi"])
+   ))
+
+#_(def example-input-1
+    (pp-str
+     [:svg/svg
+      [:svg/circle
+       {:cx 50 :cy 50 :r 50 :fill "green"}
+       ]]
+     ))
+
+#_(def example-input-1
+  (pp-str
+   '(let [s 50 c "green"]
+      [:svg/svg
+       {:height (* 2 s)
+        :width  (* 2 s)}
+       [:svg/circle
+        {:cx s :cy s :r s :fill c}
+        ]]
+      )))
+
+(def example-input-1
+  (pp-str
+   '(let [w 50 c "green"]
+      [:svg/svg
+       {:height w :width w}
+       [:svg/rect
+        {:height w :width w :fill c}
+        ]]
+      )))
+
+#_(def example-input-1
+  (pp-str
+   '[:input
+     {:on-keypress
+      (fn [e]
+        (let [v (-> e .-target .-value)]
+          (prn :v v)))
+      }
+     ]
    ))
 
 (defn view []
@@ -41,19 +103,26 @@
      #_[:button {:on-click #(eval-str "(println \"hello world!\")")} "let's compile!"]
      #_[:button {:on-click #(reset! output (eval-str @input))} "let's compile!"]
      #_[:p]
-     [:h3 "Enter to evalue cljs input"]
-     [:input
+     (rx
+      (let [out1 (:value @output)]
+        [:div
+         out1
+         [:p #_(str out1)]]))
+
+     [:h3 "Press F8 to play"]
+     [:textarea
       {:id "in1"
        :on-keypress
        (fn [e]
          (let [v (-> e .-target .-value)]
-           (prn :code (.-key e))
-           (when (= "Enter" (.-key e)) 
-             (reset! output (eval-str v)))))
+           (when (= "F8" (.-key e)) 
+             (reset! output
+                     (eval-str-2 v)))))
 
-       :value @input :type "text"}]
-     [:p] [:h3 (rx (str "" @output))]
-     [:p] (rx (:value @output))
+       :rows 10 :cols 60}
+
+      @input]
+
      ]))
 
 (set!
@@ -65,6 +134,14 @@
      (aset js/document "title" "Eval cljs in cljs")
 
      (dom/mount! root (view))
+
+
+     (dom/listen!
+      js/window "mousemove"
+      (fn [e]
+        (reset! mousexy
+         {:x (.-clientX e)
+          :y (.-clientY e)})))
 
      (.focus (js/document.getElementById "in1"))
      )))
